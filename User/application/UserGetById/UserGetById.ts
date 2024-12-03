a5619150cc6014338a34379e4111fe137e7eee79
giulianoh92 <giulianohillebrand@gmail.com>
2024-12-03 12:15:55 -0300

    feat: agregar manejo de errores en UserGetById para usuario no encontrado

--- User/application/UserGetById/UserGetById.ts
+++ User/application/UserGetById/UserGetById.ts
@@ -1,12 +1,16 @@
 import { User } from "../../domain/User";
 import { UserId } from "../../domain/UserId";
 import { UserRepository } from "../../domain/UserRepository";
+import { UserNotFoundError } from "../../domain/UserNotFoundError";
 
 export class UserGetById {
     constructor(private userRepository: UserRepository) {}
 
-    async run(id: string): Promise<User | null> {
-        const userId = new UserId(id);
-        return this.userRepository.getById(userId);
+    async run(id: string): Promise<User> {
+        const user = await this.userRepository.getById(new UserId(id));
+
+        if (!user) throw new UserNotFoundError('User not found');
+        
+        return user;
     }
 }
